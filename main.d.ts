e297e3ec18ff88d3c3192497dded5bb5bad4ae24
Joshua Maserow <joshua@softwarecreations.co.za>
2024-10-03 12:48:28 +0200

    'Added TS declarations and bumped version.'

---
+++ main.d.ts
@@ -0,0 +1,11 @@
+declare module 'esc-file-replace-substring' {
+  export const fileReplaceSubstring: (
+    filePath: string,
+    search  : string | RegExp,
+    replace : string,
+    ifNotFound?: 'append',
+    changedLogF?  : ((msg: string) => void) | null,
+    notFoundLogF? : ((msg: string) => void) | null,
+    unchangedLogF?: ((msg: string) => void) | null
+  ) => void;
+}
